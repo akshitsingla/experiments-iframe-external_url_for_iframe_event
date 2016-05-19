790668edaf5eb5fe9f4f35c28b78656d70cf1c66
unknown <asingla@BLRL31980.iis.amadeus.net>
2016-05-19 23:43:04 +0530

    trying if parent is accessible on btn-click

---
+++ app2.js
@@ -0,0 +1,17 @@
+var isApp = false;
+
+function btnClick() {
+  if (parent.isApp && parent.isApp == true) {
+  	/* RECEIVING MSG WITHIN IFRAME */
+      function listener(event) {
+          document.write (event.data);
+          console.log(JSON.parse(event.data));
+      }
+
+      if (window.addEventListener){       
+        addEventListener("message", listener, false)      
+      } else {        
+        attachEvent("onmessage", listener)        
+      }
+  }
+}
