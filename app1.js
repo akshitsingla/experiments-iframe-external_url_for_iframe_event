464982ae39b5e3e3ec11f243a027999b5102578f
unknown <asingla@BLRL31980.iis.amadeus.net>
2016-05-19 22:28:39 +0530

    modification to avoid CORS issue.

---
+++ app1.js
@@ -0,0 +1,11 @@
+/* RECEIVING MSG WITHIN IFRAME */
+function listener(event) {
+    document.write (event.data);
+    console.log(JSON.parse(event.data));
+}
+
+if (window.addEventListener){       
+  addEventListener("message", listener, false)      
+} else {        
+  attachEvent("onmessage", listener)        
+}
